628036328214904a9ef76b6531c1a08013f86235
Code Sandbox <hello@codesandbox.io>
2023-10-31 08:40:03 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.127897e08e6bf6b2d7c7592337793d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57638a30ea9075de62f4b6c9ff8ed3b9",
+    "url": "https://imoan723.github.io/csb-ryr5j2/index.html"
+  },
+  {
+    "revision": "c9edd1fc594891db2dad",
+    "url": "https://imoan723.github.io/csb-ryr5j2/static/css/2.cd87844c.chunk.css"
+  },
+  {
+    "revision": "daf8928cd73434b6f4a6",
+    "url": "https://imoan723.github.io/csb-ryr5j2/static/css/main.73c845c0.chunk.css"
+  },
+  {
+    "revision": "c9edd1fc594891db2dad",
+    "url": "https://imoan723.github.io/csb-ryr5j2/static/js/2.5f007836.chunk.js"
+  },
+  {
+    "revision": "daf8928cd73434b6f4a6",
+    "url": "https://imoan723.github.io/csb-ryr5j2/static/js/main.35aa11d5.chunk.js"
+  },
+  {
+    "revision": "95fca9975d6fc5cb4bb8",
+    "url": "https://imoan723.github.io/csb-ryr5j2/static/js/runtime~main.c44f6b76.js"
+  }
+]);
